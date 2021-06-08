97dddce3e7f15c06bd3efa5854a1ec1acf469775
sachchin06 <sachchin06@gmail.com>
2021-06-08 20:18:32 +0530

    find By categiry id method added

--- onlinebookstore/src/main/java/com/ram/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/ram/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package com.ram.onlinebookstore.repository;
 
 import com.ram.onlinebookstore.entity.Book;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.data.domain.Pageable;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @RestResource(path = "categoryId")
+    Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
